740b7b2cd348034bbf5b2cca844b1a26f20fd7f6
Tejashwini Nellutla <nellutlatejashwini11@gmail.com>
2025-08-29 23:41:03 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,214 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import HomePage from "./pages/HomePage";
-// import Login from "./pages/auth/Login";
-// import FrontPage from "./pages/frontPage";
-// import Register from "./pages/auth/Register";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import ProtectedRoute from "./components/Routes/ProtectedRoute";
-// import PublicRoute from "./components/Routes/PublicRoute";
-// import Donar from "./pages/Dashboard/Donar";
-// import Hospitals from "./pages/Dashboard/Hospitals";
-// import OrganisationPage from "./pages/Dashboard/OrganisationPage";
-// import Consumer from "./pages/Dashboard/Consumer";
-// import Donation from "./pages/Donation";
-// import Analytics from "./pages/Dashboard/Analytics";
-// import DonarList from "./pages/Admin/DonarList";
-// import HospitalList from "./pages/Admin/HospitalList";
-// import OrgList from "./pages/Admin/OrgList";
-// import AdminHome from "./pages/Admin/AdminHome";
-// import Contact from "./pages/contactus";
-// import Navbar from "./pages/Navbar";
-// import Footer from "./pages/Footer";
-// import AboutUs from "./pages/AboutUs";
-// import VolunteerForm from "./pages/Volunteer";
-// import Email from "./pages/email";
-// import Header from "./components/shared/Layout/Header";
-// import DonorHome from "./pages/Admin/donorHome";
-// import WeatherForecast from './components/weatherforecast/WeatherForecast';
-
-
-// function App() {
-//   return (
-//     <>
-//       <ToastContainer />
-//       <Routes>
-    
-//         <Route
-//           path="/volunteer"
-//           element={
-//             <ProtectedRoute>
-//               <Header/>
-//               <VolunteerForm />
-//             </ProtectedRoute>
-            
-//           }
-//         />
-        
-//         <Route
-//           path="/email"
-//           element={<Email />}
-//         />
-//         <Route
-//           path="/admin"
-//           element={
-//             <ProtectedRoute>
-//               <AdminHome />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/donorhome"
-//           element={
-//             <ProtectedRoute>
-//               <DonorHome />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/donar-list"
-//           element={
-//             <ProtectedRoute>
-//               <DonarList />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/home"
-//            element={
-//           <>
-//             <Header/>
-//               <FrontPage />
-//               </>
-            
-//           }
-//         />
-//         <Route
-//           path="/hospital-list"
-//           element={
-//             <ProtectedRoute>
-//               <HospitalList />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/org-list"
-//           element={
-//             <ProtectedRoute>
-//               <OrgList />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/hospital"
-//           element={
-//             <ProtectedRoute>
-//               <Hospitals />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/analytics"
-//           element={
-//             <ProtectedRoute>
-//               <Analytics />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/consumer"
-//           element={
-//             <ProtectedRoute>
-//               <Consumer />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/donation"
-//           element={
-//             <ProtectedRoute>
-//               <Donation />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/orgnaisation"
-//           element={
-//             <ProtectedRoute>
-//               <OrganisationPage />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/donar"
-//           element={
-//             <ProtectedRoute>
-//               <Donar />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/"
-//           element={
-//             <ProtectedRoute>
-//               <HomePage />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/login"
-//           element={
-//             <PublicRoute>
-//               <Login />
-//             </PublicRoute>
-//           }
-//         />
-    
-//         <Route
-//           path="/register"
-//           element={
-//             <PublicRoute>
-//               <Register />
-//             </PublicRoute>
-//           }
-//         />
-       
-//         <Route
-//           path="/aboutus"
-//           element={
-//             <>
-//               <Header/>
-//               <AboutUs />
-//               </>
-           
-//           }
-//         />
-//         <Route
-//          path="/contactus"
-//           element={
-//             <>
-//               <Header />
-//               <Contact />
-//               </>
-           
-//           }
-//       />
-//        {/* Add the WeatherForecast Route Here */}
-//        <Route
-//           path="/weather-forecast"
-//           element={<WeatherForecast />}
-//         />
-      
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
-
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/auth/Login";
@@ -218,29 +7,17 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ProtectedRoute from "./components/Routes/ProtectedRoute";
 import PublicRoute from "./components/Routes/PublicRoute";
-import Donar from "./pages/Dashboard/Donar";
-import Hospitals from "./pages/Dashboard/Hospitals";
-import OrganisationPage from "./pages/Dashboard/OrganisationPage";
-import Consumer from "./pages/Dashboard/Consumer";
-import Donation from "./pages/Donation";
 import Analytics from "./pages/Dashboard/Analytics";
-import DonarList from "./pages/Admin/DonarList";
-import HospitalList from "./pages/Admin/HospitalList";
-import OrgList from "./pages/Admin/OrgList";
 import AdminHome from "./pages/Admin/AdminHome";
 import Contact from "./pages/contactus";
-import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
-import AboutUs from "./pages/AboutUs";
 import VolunteerForm from "./pages/Volunteer";
 import Email from "./pages/email";
-import SupplierDocuments from "./pages/SupplierDocuments";
 import AdminUpload from "./pages/Admin/AdminUpload";
 import Header from "./components/shared/Layout/Header";
-import DonorHome from "./pages/Admin/donorHome";
+import DonorHome from "./pages/Admin/SupplierHome";
 import WeatherForecast from './components/weatherforecast/WeatherForecast';
 import CropSalesAnalytics1 from "./pages/Dashboard/CropSalesAnalytics1";
-import DiseasePrediction from "./pages/DiseasePrediction";
 import CropPrediction from './pages/CropPrediction';
 
 
@@ -272,18 +49,10 @@ function App() {
           }
         />
         <Route
-          path="/donorhome"
+          path="/supplierhome"
           element={
             <ProtectedRoute>
-              <DonorHome />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/donar-list"
-          element={
-            <ProtectedRoute>
-              <DonarList />
+              <SupplierHome />
             </ProtectedRoute>
           }
         />
@@ -297,78 +66,16 @@ function App() {
             </>
           }
         />
+
         <Route
-          path="/hospital-list"
-          element={
-            <ProtectedRoute>
-              <HospitalList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/org-list"
-          element={
-            <ProtectedRoute>
-              <OrgList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/hospital"
-          element={
-            <ProtectedRoute>
-              <Hospitals />
-            </ProtectedRoute>
-          }
-        />
-        {/* <Route
           path="/analytics"
           element={
-            <ProtectedRoute>
+            <Route>
               <Analytics />
-            </ProtectedRoute>
-          }
-        /> */}
-         {/* <Route
-          path="/analytics"
-          element={
-            <>
-            <AboutUs/>
-            </>
-          }
-        /> */}
-        <Route
-          path="/consumer"
-          element={
-            <ProtectedRoute>
-              <Consumer />
-            </ProtectedRoute>
+            </Route>
           }
         />
-        <Route
-          path="/donation"
-          element={
-            <ProtectedRoute>
-              <Donation />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/organisation"
-          element={
-            <ProtectedRoute>
-              <OrganisationPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/donar"
-          element={
-            <ProtectedRoute>
-              <Donar />
-            </ProtectedRoute>
-          }
-        />
+        
         <Route
           path="/"
           element={
@@ -393,16 +100,7 @@ function App() {
             </PublicRoute>
           }
         />
-        <Route
-          path="/aboutus"
-          element={
-            <>
-              <Header />
-              <CropSalesAnalytics1 />
-              <Footer />
-            </>
-          }
-        />
+        
         <Route
           path="/contactus"
           element={
@@ -421,14 +119,7 @@ function App() {
           path="/admin-upload"
           element={<AdminUpload />}
         />
-        <Route
-          path="/supplier-list"
-          element={<SupplierDocuments />}
-        />
-        <Route
-          path="/disease-prediction"
-          element={<DiseasePrediction />}
-        />
+              
         <Route
           path="/crop-prediction"
           element={
