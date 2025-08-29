b4a41b5dd0de551df8163bc77f58e448c2aeb542
Tejashwini Nellutla <nellutlatejashwini11@gmail.com>
2025-08-29 23:24:52 +0530

    Update Layout.js

--- client/src/components/shared/Layout/Layout.js
+++ client/src/components/shared/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
@@ -9,9 +8,7 @@ const Layout = ({ children }) => {
         <Header />
       </div>
       <div className="row g-0">
-        <div className="col-md-3">
-          <Sidebar />
-        </div>
+
         <div className="col-md-9">{children}</div>
       </div>
     </>
